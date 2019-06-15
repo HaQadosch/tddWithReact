11da108bdcabb3dff509f0b95bd529dd89a3b8ef
HaQadosch Berraka <haqadosch@pm.me>
2019-06-15 15:30:26 +0100

    remove test file

--- my-little-pony/src/stories/RedPony.stories.tsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-import { storiesOf } from '@storybook/react';
-import { RedPony } from '../components/RedPony';
-import '../components/RedPony.css';
-import '../index.css';
-
-storiesOf('RedPony', module).add('default', () => <RedPony />);
