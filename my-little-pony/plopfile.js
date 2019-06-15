113aec29101ae952dcedae9a1098f9d8b415be68
HaQadosch Berraka <haqadosch@pm.me>
2019-06-15 15:29:09 +0100

    plop file for command rcts

---
+++ my-little-pony/plopfile.js
@@ -0,0 +1,34 @@
+module.exports = plop => {
+  plop.setGenerator('rcts', {
+    description: 'Create a component',
+    prompts: [
+      {
+        type: 'input',
+        name: 'name',
+        message: 'What is your component name?',
+      },
+    ],
+    actions: [
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}.tsx',
+        templateFile: 'plops/Component.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/components/{{pascalCase name}}.css',
+        templateFile: 'plops/Component.css.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/__tests__/{{pascalCase name}}.test.tsx',
+        templateFile: 'plops/Component.test.tsx.hbs',
+      },
+      {
+        type: 'add',
+        path: 'src/stories/{{pascalCase name}}.stories.tsx',
+        templateFile: 'plops/Component.stories.tsx.hbs',
+      },
+    ],
+  });
+};
