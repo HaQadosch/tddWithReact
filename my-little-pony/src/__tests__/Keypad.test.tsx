7f7e8df66972333a6937e7a7b8cfce8c8c73c1fe
HaQadosch <hadadosch@pm.me>
2019-06-17 11:34:57 +0100

    Keypad test was missing displayValue prop.

--- my-little-pony/src/__tests__/Keypad.test.tsx
+++ my-little-pony/src/__tests__/Keypad.test.tsx
@@ -13,18 +13,22 @@ const selectOperator = jest.fn(() => '');
 describe('<Keypad />', () => {
   test('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Keypad setDisplayValue={setDisplayValue} selectOperator={selectOperator} />, div);
+    ReactDOM.render(<Keypad setDisplayValue={setDisplayValue} selectOperator={selectOperator} displayValue='1' />, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
   test('loads and display <Keypad />', () => {
-    const { getByTestId } = render(<Keypad setDisplayValue={setDisplayValue} selectOperator={selectOperator} />);
+    const { getByTestId } = render(
+      <Keypad setDisplayValue={setDisplayValue} selectOperator={selectOperator} displayValue='1' />,
+    );
     const keypad = getByTestId('keypad');
     expect(keypad).toBeInTheDocument();
   });
 
   test('fire event when clicked', () => {
-    const { getAllByTestId } = render(<Keypad setDisplayValue={setDisplayValue} selectOperator={selectOperator} />);
+    const { getAllByTestId } = render(
+      <Keypad setDisplayValue={setDisplayValue} selectOperator={selectOperator} displayValue='1' />,
+    );
     const calc = getAllByTestId('key');
     fireEvent.click(calc[0]);
     expect(setDisplayValue).toBeCalled();
